5b73a0f024d6962e41e453dbc79818a6c2fcf67b
Marco GILLES <marco.gilles.ext@orange.com>
2024-11-04 17:04:08 +0100

    fix remove unused import

--- litellm/proxy/auth/handle_jwt.py
+++ litellm/proxy/auth/handle_jwt.py
@@ -13,7 +13,6 @@ from typing import Optional, cast
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
-import jwt.algorithms
 
 from litellm._logging import verbose_proxy_logger
 from litellm.caching.caching import DualCache
